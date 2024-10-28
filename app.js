515d149a01331b108adbecaac3dbe6fe4d9d0443
Rahul Ram P <psrahulram@gmail.com>
2024-10-28 16:21:37 +0530

    episode-01

---
+++ app.js
@@ -0,0 +1,14 @@
+const element = React.createElement(
+  "div",
+  { class: "first" },
+  React.createElement("div", { class: "second" }, [
+    React.createElement("h1", {}, "Here we go.."),
+    React.createElement("h3", {}, "BITW.."),
+  ]),
+  React.createElement("div", { class: "next" }, [
+    React.createElement("h1", {}, "FINISH THE STORY"),
+    React.createElement("h2", {}, "FINISH THE STORY"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(element);
